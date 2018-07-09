711d67775763671030d79f5f6e07c17e218e06b7
Zyk <deneux.j@gmail.com>
2018-07-09 17:19:26 +0200

    Modification Lundi,
    Changement de stratégie
    -> génération des nombres présent dans temp;
    -> placement des nombres dans combinaison;
    
    A faire :
    
    Revoir peut-être le système et le faire avec une seule linkedlist.
    
    -> Selon le nombre présent, ajouter le x fois le nombre a la
    combinaison,
    -> Si non présent, incrémenter la couleur,
    -> Déplacer les pions en fonction si la position a été testé ou non.
    -> Si nombre présent et incrémentation du nombre de boule bien placé,
    pion ok
    sinon modifier....

--- src/testuni/testdeux.java
+++ src/testuni/testdeux.java
@@ -1,15 +1,20 @@
 package testuni;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class testdeux {
 
 	int numberOBalls = 4; // indique le nombre de boule a trouver.
-	public Integer[] secret = {1,4,1,2}; // Le nombre a trouver.
-	public Integer[] temp  = new Integer[numberOBalls];
-	public Integer[] combinaison = new Integer[numberOBalls];
-
-
+	public Integer[] secret = {2,4,6,2}; // Le nombre a trouver.
+	public Integer[] testcolor  = new Integer[numberOBalls];
+	boolean check = true;
+	public LinkedList<Integer> temp = new LinkedList<Integer>();
+	public LinkedList<Integer> combinaison = new LinkedList<Integer>();
 	int COLORS = 8; // indique le nombre de couleur maximum
 	int present = 0;
 	int inposition = 0;
@@ -17,73 +22,121 @@ public class testdeux {
 	int boucle = 0;
 	int index = 0;
 	int ballFound = 0;
+	int pos = 0;
 
 
 	public void duelMasterMind (Integer[]secret,int COLORS, int numberOBalls) {
 
-		int nextColor = color+1;
-		int searchColor = 0;
+		int wPosition = 0;
+		int gPosition = 0;
 
-		for(int i = 0; i < numberOBalls; i++) {
-			temp[i]=color;
-			combinaison[i]=0;
+		for(int y = 0; y < numberOBalls; y++) {
+			testcolor[y]=color;
+			combinaison.add(1);
+			temp.add(0);
 		}
-		
-		boucle++;
-		inposition =compareinposition(secret, temp, numberOBalls);
-		present = comparepresent(secret, temp, numberOBalls)- inposition;
-		System.out.println(boucle);
-		System.out.println(+inposition +" position || "+present +" présent");
-		System.out.println(Arrays.toString(combinaison)+ " comb");
-		System.out.println(Arrays.toString(temp)+ " temp");
-		
-		do {
+       boucle++;
+		do { 
 
-			for (int i = 0; i < numberOBalls; i++) {
-				int position = 0;
-				
-				if(present > 0) 
-				if(combinaison[i]!=0) {
-					temp[i]= combinaison[i];
-				}else {
-					temp[i]=color;
-					if(present > 0){
-						searchColor = color;
-						for(int x = 0; x < numberOBalls; x++) {
-							if(temp[position] != secret[i]){
-								temp[position] = searchColor;
-					            position++;
-							}else if(temp[position]==secret[i]) {
-									combinaison[position]=temp[position];
-								}
-							}
-						}
-					}
-				boucle++;
-				color++;
-				inposition =compareinposition(secret, temp, numberOBalls);
-				present = comparepresent(secret, temp, numberOBalls)- inposition;
-				System.out.println(boucle);
-				System.out.println(+inposition +" position || "+present +" présent");
-				System.out.println(Arrays.toString(combinaison)+ " comb");
-				System.out.println(Arrays.toString(temp)+ " temp");
-				}
-	
-		} while (compareinposition(secret, temp, numberOBalls) <= numberOBalls && boucle < 5);
+			for(int i = 0; i < numberOBalls; i++) {
+				testcolor[i]=color;
+				if(check == true) {
+					temp.set(i, color);
+				}	
+			}
+
+			inposition = compareinposition(secret, testcolor, numberOBalls);
+			present = comparepresent(secret, testcolor, numberOBalls)- inposition;
+			int ballcolor = inposition + present;
+
+			if(ballcolor > 0) {
+				addToCombinaison(temp,color,ballcolor);
+			}
+			moveBallCombinaison2(temp,secret);
+			gPosition = compareinposition(secret, combinaison, numberOBalls);
+			wPosition = comparepresent(secret, temp, numberOBalls) - gPosition;
+			System.out.println(+boucle +" boucle ");
+			System.out.println(+gPosition +" position || "+wPosition +" présent");
+			System.out.println(temp+ " temp");
+			System.out.println(combinaison + " comb");
+			boucle++;
+			color++;
+
+		} while (compareinposition(secret, combinaison, numberOBalls) < numberOBalls && boucle < 15);
 	}
 
+	private int compareinposition(Integer[] secret, LinkedList<Integer> temp, int numberOBalls) {
 
-	public int compareinposition(Integer[] secret,Integer[] temp, int numberOBalls) {
 		int inposition = 0;
 		for (int i = 0; i < numberOBalls; i++) {
-			if (secret[i] == temp[i]) {
+			if (temp.get(i).equals(secret[i])) {
 				inposition++;
 			}
 		}
 		return inposition;
 	}
 
-	public int comparepresent(Integer[] secret, Integer[] temp, int numberOBalls) {
+	private int comparepresent(Integer[] secret, LinkedList<Integer> temp, int numberOBalls) {
+		int ispresent = 0;
+		boolean found = false;
+
+		for (int i = 0; i < numberOBalls; i++) {
+			int j = 0;
+			found = false;
+			do {
+				if (secret[i] == temp.get(j)) {
+					ispresent++;
+					found = true;
+				}
+				j++;	
+			} while (j < numberOBalls &&!found);
+		}
+		return ispresent;
+	}
+
+
+	public void addToCombinaison (LinkedList<Integer> temp, int color, int ballcolor) {
+
+		int x = 0;
+		while(ballcolor > x) {
+			temp.set(pos,color);
+			check = false;
+			x++;
+			ballFound++;
+		}
+	}
+
+	public LinkedList<Integer> moveBallCombinaison2 (LinkedList<Integer> temp,Integer[]secret) {
+
+		int last = numberOBalls;
+		boolean found = false;
+		 
+		Collections.rotate(temp, 1);
+		for(int i = 0; i < numberOBalls; i++) {
+			if(temp.get(index)==secret[i]) {
+				combinaison.set(i,secret[i]);
+				found = true;
+			}else{
+				if(found = false) {
+					combinaison.set(i,color++);
+				}
+			}
+		}	
+		return combinaison;
+	}
+
+
+	public int compareinposition(Integer[] secret,Integer[] testcolor, int numberOBalls) {
+		int inposition = 0;
+		for (int i = 0; i < numberOBalls; i++) {
+			if (secret[i] == testcolor[i]) {
+				inposition++;
+			}
+		}
+		return inposition;
+	}
+
+	public int comparepresent(Integer[] secret, Integer[] testcolor, int numberOBalls) {
 
 		int ispresent = 0;
 		boolean found = false;
@@ -92,12 +145,11 @@ public class testdeux {
 			int j = 0;
 			found = false;
 			do {
-				if (secret[i] == temp[j]) {
+				if (secret[i] == testcolor[j]) {
 					ispresent++;
-					temp[j] = color;
 					found = true;
 				}
-				j++;
+				j++;	
 			} while (j < numberOBalls &&!found);
 		}
 		return ispresent;
--- src/testuni/tryduel.java
+++ src/testuni/tryduel.java
@@ -4,86 +4,121 @@ import java.util.Arrays;
 
 public class tryduel extends TestUni {
 
-	Integer[] secret = {8,7,3,8};
-	Integer[] combinaison = new Integer[4];
-	int present = 0;
-	int inposition= 0;
-	int colors = 1;
-	int boucle = 0;
+	public int[] secret = {5,4,3,7};
 
-	public void jeu (Integer[]combinaison, int colors, int boucle) {
+	int COLORS = 8;
+	int numberOBalls = 4;
+	public void jeu(int[] secret, int COLORS, int numberOBalls) {
 
-		boolean isinposition = false;
-		boolean ispresent = false;
-		Integer[] temp = new Integer[4];
-		int p =0;
+		int[] temp = new int[numberOBalls]; //Tableau contenant la combinaison
+		int[] combinaison = new int[numberOBalls]; // Tableau contenant les couleurs
+		int color = 1; // Couleur de départ
+		int inposition = 0; // pion présent à la bonne position
+		int ispresent = 0;  // pion présent mais mal placé
+		int ballFound = 0; // Nombre de boules de couleurs dont on a trouvé la bonne position
+		int boucle = 0;    // nombres de tentatives
+		int position = 0;  // indique la position de la boule.
 
-		for(int x =0;x <4;x++) {
-			temp[x] = colors;
-			combinaison[x] = temp[x];
-		}
+		/*
+		 * Boucle qui se répète jusqu'à la résolution du jeu.
+		 */
+		while (compareinposition(secret, temp, numberOBalls) <= numberOBalls){
 
-		do {
 
-			if(boucle > 0 && !ispresent) colors++;
-
-			for(int i = 0; i <4; i++) {
-				isinposition = (secret[i]==combinaison[i]);
-				for(int y = 0; y < 4; y++) {
-					ispresent =(secret[i]==combinaison[y] && (secret[i]!=combinaison[i]));
-					if(isinposition) {
-					combinaison[i] = temp[i];
-					}
-					if(!isinposition && !ispresent) {
-						temp[i]= colors; 
-					}
-					if(!isinposition && ispresent) {
-						p = colors;
-						temp[i] = p;
-					}
-					if(ispresent && (!isinposition)) {
-						temp[i]=p;
-					}
-				}
-				combinaison[i] = temp[i];
+			for (int i = 0; i < numberOBalls; i++) {  // Création d'une combinaison pour déterminer ou non présence d'une couleur
+				if (combinaison[i] == 0)
+					temp[i] = color;
+				else
+					temp[i] = combinaison[i];
 			}
- 
-			comparepresent(secret, combinaison);  //Comparaison des résultats
-			compareinpositon(secret, combinaison);
-
-			System.out.println(inposition+" positions" + "|" + present+" presents"); // Affichage console
-			System.out.println(Arrays.toString(combinaison)+" combi");
-			//System.out.println(Arrays.toString(temp)+" temp");
-
+		
 			boucle++;
+			
+			inposition = compareinposition(secret, temp, numberOBalls); // Détermine les pions bien placés
+			ispresent = comparepresent(secret, temp, numberOBalls) - compareinposition(secret, temp, numberOBalls); // détermine les pions présent
+		
+			int Balls = inposition - ballFound; // nombre de boule
 
-		} while ((secret != combinaison) && (boucle < 10));
+			if (Balls >= 1 && color <= (COLORS + 1)) {
+				
+				for (int x = 1; x <= Balls; x++) {
+					ispresent = 1;
+					position = 0;
 
-	}
+					while (ispresent > 0) {
+						while ((position < numberOBalls) && combinaison[position] != 0) // Nouvelle combinaison qui cherche position de la couleur actuelle.
+							position++;
 
-	public int comparepresent (Integer[]secret,Integer[]combinaison) {
-		present = 0;
+						for (int i = 0; i < numberOBalls; i++) { 
+							if (combinaison[i] == 0) // Si on ne connait pas la couleur de la case courante.
+							{
+								if (i != position) {// Si la case n'est pas une case testé, color++
+									temp[i] = color + 1;
+								}else {
+									temp[i] = color;
+								}
+							} else
+								temp[i] = combinaison[i];
+						}
 
-		for(int i = 0; i < 4; i++) {
-			for(int y = 0; y < 4; y++) {
-				if(secret[i]==combinaison[y] && secret[i]!=combinaison[i])
-					present++;
+						boucle++;
+						inposition =compareinposition(secret, temp, numberOBalls);
+						ispresent = comparepresent(secret, temp, numberOBalls)- inposition;
+						
+						position++;
+
+					}
+
+					// A la sortie de la boucle, on a la position de la boule de
+					// couleur -> pos - 1
+					// On ajoute donc cette boule ˆ la combinaison contenant les
+					// boules TrouvŽes
+					combinaison[position - 1] = color;
+
+					// on incrŽmente le nombre de boule trouvŽes
+					ballFound++;
+					System.out.println(+boucle);
+					System.out.println(Arrays.toString(temp));
+					System.out.println(Arrays.toString(combinaison));
+					System.out.println(" --> "+inposition+" biens placŽes, "+ispresent+" mal placŽes.");
+				}
 			}
+			color++;
 		}
-		return present; 	
 	}
-
-	public int compareinpositon(Integer[]secret,Integer[]combinaison) {
-		inposition = 0;
-		for(int i = 0; i < 4; i++) {
-			if (secret[i]==combinaison[i]) {
+	public int compareinposition(int[] secret, int[] temp, int numberOBalls) {
+		int inposition = 0;
+		for (int i = 0; i < numberOBalls; i++) {
+			if (secret[i] == temp[i]) {
 				inposition++;
 			}
 		}
 		return inposition;
 	}
 
+
+	public int comparepresent(int[] secret, int[] temp, int numberOBalls) {
+
+		int ispresent = 0;
+		boolean found = false;
+
+		for (int i = 0; i < numberOBalls; i++) {
+			int j = 0;
+			found = false;
+			do {
+				if (secret[i] == temp[j]) {
+					ispresent++;
+					temp[j] = 0;
+					found = true;
+				}
+				j++;
+			} while (j < numberOBalls &&!found);
+		}
+		return ispresent;
+	}
+	
+
 	public void tryduel() {
-		jeu(combinaison,colors,boucle);
+		jeu(secret,COLORS,numberOBalls);
 	}
 }
